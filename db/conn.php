f97047cc0d0473ce2b69f2be89bb9d583bd44f0e
Sandra Spaulding <sandyps21@gmail.com>
2020-11-11 05:26:42 -0500

    Update conn.php

--- db/conn.php
+++ db/conn.php
@@ -1,8 +1,16 @@
 <?php
-    $host = '127.0.0.1';
-    $db = 'attendance_db';
-    $user = 'root';
-    $pass = '';
+    //Development Connection
+   // $host = '127.0.0.1';
+   // $db = 'attendance_db';
+   // $user = 'root';
+   // $pass = '';
+   // $charset = 'utf8mb4';
+
+   //Remote database connection
+    $host = 'remotemysql.com';
+    $db = 'cPDneHmx2c';
+    $user = 'cPDneHmx2c';
+    $pass = 'PzYuW8PPtt';
     $charset = 'utf8mb4';
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
