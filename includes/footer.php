2436a57712ce4081ca24aaa3dfa86975a3314e71
Sandra Spaulding <sandyps21@gmail.com>
2020-11-16 01:39:10 -0500

    Updated header and footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,6 +1,6 @@
-<div id="footer">
-    <?php echo 'Copyright ' . date('y'); ?>
-    </div>
+<div id="footer" class="p-3 bg-info text-white fixed-bottom">
+<p class="text-center">Copyright &copy; -IT Conference Attendance System <?php echo date('Y'); ?></p>
+</div>
 
 </div>
 
--- includes/header.php
+++ includes/header.php
@@ -22,20 +22,18 @@
     <title>Attendance - <?php echo $title ?></title>
   </head>
   <body>
-    <div class="container">
-
-    <nav class="navbar navbar-expand-lg navbar-dark bg-info">
+  <nav class="navbar navbar-expand-lg navbar-dark bg-info">
   <a class="navbar-brand" href="index.php">IT Conference</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+  <div class="collapse navbar-collapse container" id="navbarNavAltMarkup">
     <div class="navbar-nav mr-auto">
       <a class="nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
       <a class="nav-link" href="viewrecords.php">View Attendees</a>
     </div>
 
-    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="collapse navbar-collapse " id="navbarNavAltMarkup">
     <div class="navbar-nav ml-auto">
     <?php 
       if(!isset($_SESSION['userid'])){
@@ -54,4 +52,7 @@
 
   </div>
 </nav>
+    <div class="container">
+
+    
 <br/>
